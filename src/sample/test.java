349ba0eeebb26962963e4e4acb23b2dbeaf3a464
Nourfi33 <nicolas.gras@etu.u-bordeaux.fr>
2016-12-01 22:57:16 +0100

    Ajout des tests jUnit

---
+++ src/sample/test.java
@@ -0,0 +1,17 @@
+package sample;
+
+import org.junit.Test;
+
+public class test {
+    @Test
+    public void testModel(){
+
+
+    }
+
+    @Test
+    public void name() throws Exception {
+
+
+    }
+}
